cb7fd8f7290a237305b94ac5b31f1a6fe484bd87
Sergey Titov <titovsb@gmail.com>
2021-04-19 01:37:47 +0300

    Lesson4 homework repaired

--- lesson4/list1.py
+++ lesson4/list1.py
@@ -3,7 +3,13 @@
     Примечание. Списки создайте вручную, например так:
 '''
 
-my_list_1 = [2, 5, 8, 2, 12, 12, 4]
+my_list_1 = [5, 5, 2, 2, 5, 8, 8, 2, 12, 12, 4]
 my_list_2 = [2, 7, 12, 3]
 
-print(set(my_list_1) - set(my_list_2))
+#print(list(set(my_list_1) - set(my_list_2)))
+
+for e in my_list_1[:]:
+    if e in my_list_2:
+        my_list_1.remove(e)
+
+print(my_list_1)
